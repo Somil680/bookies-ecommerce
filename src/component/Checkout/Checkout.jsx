e325dfb78edbebb63e3ee4e6cf453e4aa2969074
Somil Agrawal <somilagrawal680@gmail.com>
2023-02-14 01:26:22 +0530

    update

--- src/component/Checkout/Checkout.jsx
+++ src/component/Checkout/Checkout.jsx
@@ -1,4 +1,6 @@
- import React from 'react';
+import React from 'react';
+//  import { useCallback } from "react";
+// import useRazorpay from "react-razorpay";
 import { useCart } from '../../context';
 import { Footer } from '../Footer/footer';
 import { Navbar } from '../Navbar/Navbar';
@@ -9,6 +11,39 @@ function Checkout() {
  const {cartState} = useCart()
     const { cart } = cartState
     console.log(cart)
+
+//  const Razorpay = useRazorpay();
+
+//   const handlePayment = useCallback(() => {
+//     // const order =  createOrder(params);
+
+//     const options: RazorpayOptions = {
+//       key: "YOUR_KEY_ID",
+//       amount: "3000",
+//       currency: "INR",
+//       name: "Acme Corp",
+//       description: "Test Transaction",
+//       image: "https://example.com/your_logo",
+//     //   order_id: order.id,
+//       handler: (res) => {
+//         console.log(res);
+//       },
+//       prefill: {
+//         name: "Piyush Garg",
+//         email: "youremail@example.com",
+//         contact: "9999999999",
+//       },
+//       notes: {
+//         address: "Razorpay Corporate Office",
+//       },
+//       theme: {
+//         color: "#3399cc",
+//       },
+//     };
+
+//     const rzpay = new Razorpay(options);
+//     rzpay.open();
+//   }, [Razorpay]);
     
 return <>
 <Navbar/>
@@ -30,6 +65,7 @@ return <>
         
 <div className="Checkout-price-data">
         <Price />
+        {/* <button className='checkout-btn' onClick={handlePayment}>Check Out</button> */}
         <Link className='checkout-btn'>Check Out</Link>
 </div>     
     </div>
