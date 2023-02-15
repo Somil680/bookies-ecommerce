0d5a278860a37ff4b16b46469ae1fbfc7c7e1882
Somil Agrawal <somilagrawal680@gmail.com>
2023-02-15 19:26:00 +0530

    feat update footer.jsx

--- src/component/Footer/footer.jsx
+++ src/component/Footer/footer.jsx
@@ -12,10 +12,10 @@ function Footer() {
                  <h4 style={{margin:"0px"}}>© Bookie's 2023. All rights reserved. Site – Managed by Somil Agrawal.</h4>
             </div>
             <div >
-            <a className="social-link" href="/"><SiGithub/></a>
-            <a className="social-link" href="/"><SiLinkedin/></a>
-            <a className="social-link" href="/"><SiTwitter/></a>
-            <a className="social-link" href="/"><SiInstagram /></a>
+<a className="social-link" href="https://github.com/Somil680"><SiGithub/></a>
+<a className="social-link" href="https://www.linkedin.com/in/somil-agrawal-0b5092189/"><SiLinkedin/></a>
+<a className="social-link" href="https://twitter.com/AgrawalSomil151"><SiTwitter/></a>
+<a className="social-link" href="https://www.instagram.com/_somil_agrawal_/"><SiInstagram /></a>
             </div>
         </div>
 </footer>
